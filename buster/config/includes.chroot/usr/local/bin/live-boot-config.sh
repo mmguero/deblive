df08d71d13b3d3ba43e9241de4c7be561948d266
cement-berries <47364876+cement-berries@users.noreply.github.com>
2019-02-19 09:46:23 -0700

    fix vte.sh on boot

--- buster/config/includes.chroot/usr/local/bin/live-boot-config.sh
+++ buster/config/includes.chroot/usr/local/bin/live-boot-config.sh
@@ -11,4 +11,11 @@ systemctl disable clamav-freshclam
 systemctl status ssh && systemctl stop ssh
 systemctl disable ssh
 
+# make symlink for vte.sh if it doesn't exist
+if [ ! -f /etc/profile.d/vte.sh ]; then
+  pushd /etc/profile.d >/dev/null 2>&1
+  ln -s "$(ls vte-*.sh | tail -n 1)" vte.sh >/dev/null 2>&1 || true
+  popd >/dev/null 2>&1
+fi
+
 exit 0
