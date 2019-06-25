7b4c6bf2b97c3b819dc8d3e1a5a36340abfdfd76
SG <13872653+mmguero@users.noreply.github.com>
2019-06-25 08:29:29 -0600

    force reprovision with -f

--- build_via_vagrant.sh
+++ build_via_vagrant.sh
@@ -20,9 +20,38 @@ function vm_execute() {
   vagrant ssh --no-tty --command "$1"
 }
 
+unset FORCE_PROVISION
+unset CONFIG_DIR
+while getopts 'fd:' OPTION; do
+  case "$OPTION" in
+    f)
+      FORCE_PROVISION=0
+      ;;
+    d)
+      CONFIG_DIR="$OPTARG"
+      ;;
+    ?)
+      echo "script usage: $(basename $0) [-f] -d config_dir" >&2
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+if [[ -z $CONFIG_DIR ]] || [[ ! -d $CONFIG_DIR ]] ; then
+  echo "script usage: $(basename $0) [-f] -d config_dir" >&2
+  exit 1
+fi
+
 pushd "$SCRIPT_PATH"/vagrant
 
 VM_NAME="$(grep "config.vm.box" Vagrantfile | tr -d "[:space:]" | sed "s/.*=//")"
+
+if [[ -n $FORCE_PROVISION ]]; then
+  echo "Destroying build machine to force provisioning..." >&2
+  vagrant destroy -f
+  sleep 1
+fi
 
 # make sure the VM is up and running, or start it otherwise
 if ! vm_is_running; then
@@ -45,8 +74,7 @@ until [ "$(vm_execute 'sudo whoami')" == "root" ] ; do
 done
 echo "SSH available." >&2
 
-
-vm_execute "sudo bash -c \"whoami && cd /iso-build && pwd && ./build.sh \\\"$@\\\"\""
+vm_execute "sudo bash -c \"whoami && cd /iso-build && pwd && ./build.sh \\\"$CONFIG_DIR\\\"\""
 
 if [[ -n $NEED_SHUTDOWN ]]; then
   echo "Shutting down $VM_NAME..." >&2
