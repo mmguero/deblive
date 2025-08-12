51d784cf629ca506bab7bd2049da3c85c3271890
Seth Grover <seth.d.grover@gmail.com>
2025-08-11 21:34:29 -0600

    don't need fasttrack yet

--- build.sh
+++ build.sh
@@ -126,7 +126,7 @@ if [ -d "$WORKDIR" ]; then
     --apt-indices false \
     --apt-source-archives false \
     --archive-areas 'main contrib non-free non-free-firmware' \
-    --debootstrap-options "--include=apt-transport-https,bc,ca-certificates,gnupg,debian-archive-keyring,fasttrack-archive-keyring,jq,openssl --no-merged-usr" \
+    --debootstrap-options "--include=apt-transport-https,bc,ca-certificates,gnupg,debian-archive-keyring,jq,openssl --no-merged-usr" \
     --apt-options "--yes --allow-downgrades --allow-remove-essential --allow-change-held-packages -oAcquire::Check-Valid-Until=false -oAPT::Default-Release=trixie"
 
   lb build 2>&1 | tee "$WORKDIR/output/$IMAGE_NAME-$IMAGE_VERSION-build.log"
